c41598a68cb884c11ea6595b852227a455ee4e05
elyeel <elyeel@yahoo.com>
2019-07-31 20:23:06 +0000

    moved connection to client

---
+++ client.js
@@ -0,0 +1,17 @@
+const net = require('net');
+const connect = function(data) {
+  const conn = net.createConnection({ 
+    host: '192.168.88.218',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); // setting encoding into human readable msg
+  // conn.on('connect', () => {
+  //   conn.write(data);
+  // })
+  conn.on('data', (data) => { //get message back from server
+    console.log('NEW MESSAGE FROM THE SERVER', data)
+  })
+  return conn;
+}
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,23 +1,23 @@
-const net = require('net');
+const connect = require('./client');
 
-/**
- * Establishes connection with the game server
- */
-const connect = function(data) {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); // setting encoding into human readable msg
-  // conn.on('connect', () => {
-  //   conn.write(data);
-  // })
-  conn.on('data', (data) => { //get message back from server
-    console.log('NEW MESSAGE FROM THE SERVER', data)
-  })
-  return conn;
-}
+// /**
+//  * Establishes connection with the game server
+//  */
+// const connect = function(data) {
+//   const conn = net.createConnection({ 
+//     host: '192.168.88.218',
+//     port: 50541
+//   });
+//   // interpret incoming data as text
+//   conn.setEncoding('utf8'); // setting encoding into human readable msg
+//   // conn.on('connect', () => {
+//   //   conn.write(data);
+//   // })
+//   conn.on('data', (data) => { //get message back from server
+//     console.log('NEW MESSAGE FROM THE SERVER', data)
+//   })
+//   return conn;
+// }
 
 
 console.log('Connecting ...');
