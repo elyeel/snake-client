7500dd17ff8039ba2d5ca67ab8d8367e196ad1e3
elyeel <elyeel@yahoo.com>
2019-07-31 22:08:19 +0000

    moved the snake up then left with delay

--- client.js
+++ client.js
@@ -18,21 +18,32 @@ const connect = function(data) {
   // })
   
 
-  stdin.on('connect', (data) => {
-    conn.write('Name: Ris');
-  })
+  // stdin.on('connect', (data) => {
+  //   conn.write('Name: Ris');
+  // })
 
   conn.on('connect', (data) => {  
     console.log("Successfully connected to game server");
-    conn.write('Name: RIS');
+    conn.write('Move: up');
+    // for (let i = 1; i <= 10 ; i++) { // moveup with timeout delay
+    //   setTimeout(() => {
+    //     conn.write('Move: left');
+    //   }, 100 * i)
+    // };
+    
+    // for (let i = 1; i <= 10 ; i++) { //move up with setInterval
+    //   setInterval(() => {
+    //     conn.write('Move: left');
+    //   }, 100 )
+    // };
   })
   conn.on('data', (data) => { //get message back from server
     console.log('NEW MESSAGE FROM THE SERVER', data)
   })
-  conn.on('end', (data) => {
-    console.log(data);
-  })
+  // conn.on('end', (data) => { trying to get msg when disconnected by the serrver but no data sent
+  //   console.log(data);
+  // })
   return conn;
-  conn.
+  conn
 }
 module.exports = connect;
