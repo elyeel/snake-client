1f7a51c95f7c14cdb897eb107650d4a45212b3a7
elyeel <elyeel@yahoo.com>
2019-07-31 21:42:52 +0000

    Added put my name on server

--- client.js
+++ client.js
@@ -1,3 +1,7 @@
+const stdin = process.stdin
+stdin.resume()
+stdin.setEncoding('utf8');
+
 const net = require('net');
 const connect = function(data) {
   const conn = net.createConnection({ 
@@ -9,9 +13,26 @@ const connect = function(data) {
   // conn.on('connect', () => {
   //   conn.write(data);
   // })
+  // conn.on('data', (data) => {
+  //   conn.write("Name: RIS");
+  // })
+  
+
+  stdin.on('connect', (data) => {
+    conn.write('Name: Ris');
+  })
+
+  conn.on('connect', (data) => {  
+    console.log("Successfully connected to game server");
+    conn.write('Name: RIS');
+  })
   conn.on('data', (data) => { //get message back from server
     console.log('NEW MESSAGE FROM THE SERVER', data)
   })
+  conn.on('end', (data) => {
+    console.log(data);
+  })
   return conn;
+  conn.
 }
 module.exports = connect;
